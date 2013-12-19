ad1282f63f3df4aba8ab77ba1296294d08e27da0
henryzlo <henryzlo@gmail.com>
2013-12-19 17:40:51 -0500

    yes

--- upload.py
+++ upload.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
+import json
 from base64 import b64encode
 from py3createtorrent import main as maketorrent
 from os.path import basename
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 import re
 
 if len(sys.argv)!=9:
@@ -24,15 +26,23 @@ b64_torrent = b64encode(f.read())
 f.close()
 
 # extract cookie args
-matches = re.match("uid=(.*)&pass=(.*)", sys.argv[2])
+#matches = re.match("uid=(.*)&pass=(.*)", sys.argv[2])
+
+if sys.argv[6].lower() == 'dataset':
+    category = 6
+elif sys.argv[6].lower() == 'paper':
+    category = 5
+else:
+    print("Category must be either 'paper' or 'dataset'")
+    exit()
 
 post_params = {
-    'uid' : matches.group(1),
-    'pass' : matches.group(2),
+    'uid' : "14",
+    'pass' : "71135221ceb8b5079cd15150d2126dcb",
     'name' : sys.argv[3],
     'authors' : sys.argv[4],
     'descr' : sys.argv[5],
-    'category' : sys.argv[6],
+    'category' : category,
     'tags' : sys.argv[7],
     'urllist' : sys.argv[8],
     'file' : b64_torrent
@@ -40,5 +50,11 @@ post_params = {
 
 data = urlencode(post_params).encode('utf-8')
 req = Request('http://academictorrents.com/api/paper', data)
-response = urlopen(req)
-the_page = response.read()
+
+try:
+    response = urlopen(req)
+except HTTPError as e:
+    print(e)
+    response = e
+
+print(response.read())
