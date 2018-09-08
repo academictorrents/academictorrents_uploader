83c48897b71fe62e19e9255124610fdc5ee69ffe
Joseph Paul Cohen <joseph@josephpcohen.com>
2018-09-08 20:59:32 +0200

    update scripts a bit

--- upload.sh
+++ upload.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-fname=$1
+file="$1"
+
+echo "$file"
+du -h "$file"
+echo listing . files
+find "$file" -name ".*"
+
 echo "Name:"
 read name
 echo "Authors:"
 read authors
 
-
-python3 $DIR/upload.py "$(cat $DIR/key)" "$fname" "$name" "$authors" "@article{,title={$name}}" dataset "" ""
+python3 $DIR/upload.py "$(cat $DIR/key)" "$file" "$name" "$authors" "@article{,title={$name}}" dataset "" ""
